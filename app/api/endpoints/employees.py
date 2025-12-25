c73ff64a1a726b8cd0eb4547c6b04f46adb28afa
Anna Levina <anna_levina@internet.ru>
2025-12-25 09:35:05 +0300

    feat: добавлен config

--- app/api/endpoints/employees.py
+++ app/api/endpoints/employees.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.crud.employees import get_employees
+from app.crud.employees import employees_crud
 from app.schemas.employees import Employee
 
 router = APIRouter()
@@ -8,4 +8,4 @@ router = APIRouter()
 
 @router.get("/", response_model=list[Employee])
 async def read_employees() -> list[Employee]:
-    return await get_employees()
+    return await employees_crud.get_all()
