228a67f89c236275c0aa1fcb92a71372e252f515
Anna Levina <anna_levina@internet.ru>
2025-12-26 03:58:16 +0300

    feat: добавлен круд для пагинации и сортировки

--- app/crud/base.py
+++ app/crud/base.py
@@ -2,6 +2,28 @@ class BaseMongoCRUD:
     def __init__(self, collection):
         self.collection = collection
 
-    async def get_all(self) -> list[dict]:
-        """Получить все документы коллекции без _id"""
-        return await self.collection.find({}, {"_id": 0}).to_list(length=None)
+    async def get_all(
+        self,
+        *,
+        limit: int,
+        page: int,
+        sort: str,
+        order: str,
+    ) -> tuple[list[dict], int]:
+        """
+        Получение списка документов с пагинацией и сортировкой
+        """
+        skip = (page - 1) * limit
+        sort_direction = 1 if order == "asc" else -1
+
+        cursor = (
+            self.collection.find({}, {"_id": 0})
+            .sort(sort, sort_direction)
+            .skip(skip)
+            .limit(limit)
+        )
+
+        items = await cursor.to_list(length=limit)
+        total = await self.collection.count_documents({})
+
+        return items, total
