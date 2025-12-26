6605e84cb483c8ac1f4abc8fddab3a1106197fa3
Anna Levina <anna_levina@internet.ru>
2025-12-26 03:58:45 +0300

    feat: добавлен роутер для пагинации и сортировки

--- app/api/endpoints/employees.py
+++ app/api/endpoints/employees.py
@@ -1,11 +1,36 @@
-from fastapi import APIRouter
+from math import ceil
+
+from fastapi import APIRouter, Depends
 
 from app.crud.employees import employees_crud
-from app.schemas.employees import Employee
+from app.schemas.employees import EmployeeListResponse
+from app.schemas.pagination import PaginatedParams, PaginationResponse
 
 router = APIRouter()
 
 
-@router.get("/", response_model=list[Employee])
-async def read_employees() -> list[Employee]:
-    return await employees_crud.get_all()
+@router.get("/", response_model=EmployeeListResponse)
+async def get_employees(
+    pagination: PaginatedParams = Depends(PaginatedParams.as_query()),
+) -> EmployeeListResponse:
+
+    items, total = await employees_crud.get_all(
+        limit=pagination.limit,
+        page=pagination.page,
+        sort=pagination.sort,
+        order=pagination.order,
+    )
+
+    last_page = ceil(total / pagination.limit) if total else 1
+
+    return EmployeeListResponse(
+        data=items,
+        pagination=PaginationResponse(
+            total=total,
+            limit=pagination.limit,
+            current_page=pagination.page,
+            last_page=last_page,
+            sort=pagination.sort,
+            order=pagination.order,
+        ),
+    )
