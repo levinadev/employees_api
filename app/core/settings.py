cba19ec8c00e5eded1d26da3def10bd5b72ab548
Anna Levina <anna_levina@internet.ru>
2025-12-28 19:51:02 +0300

    ref: вынесены тестовые настрйоки

--- app/core/settings.py
+++ app/core/settings.py
@@ -9,6 +9,12 @@ class Settings(BaseSettings):
     MONGO_PORT: int = 27017
     MONGO_DB: str = "employees_db"
 
+    TEST_MONGO_HOST: str = "localhost"
+    TEST_MONGO_PORT: int = 27018
+    TEST_MONGO_DB: str = "test_db"
+    TEST_DATA_FILE: str = "app/data/employees.json"
+    TEST_BASE_URL: str = "http://web:8000"
+
     APP_HOST: str = "0.0.0.0"
     APP_PORT: int = 8000
 
